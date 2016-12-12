a29578b8773186fd281434a1750e797b8a11091e
Igor Martire <igormartire@berkeley.edu>
2016-12-12 21:31:52 -0200

    Implementação inicial da classe IncluirMaterial

--- src/com/igormartire/iu/IncluirMaterial.java
+++ src/com/igormartire/iu/IncluirMaterial.java
@@ -1,6 +1,29 @@
 package com.igormartire.iu;
 
+import com.igormartire.dominio.DAO;
+import com.igormartire.dominio.Material;
+
 public class IncluirMaterial {
+
+    private DAO<Material> daoMaterial;
+
+    IncluirMaterial(DAO<Material> daoMaterial) {
+        this.daoMaterial = daoMaterial;
+    }
+
+    public void executar() {
+        Material m = getMaterial();
+        boolean incluido = this.daoMaterial.incluir(m);
+        if (incluido) {
+            System.out.println("Material incluído.");
+        } else {
+            System.out.println("Falha na inclusão do material.");
+        }
+    }
+
+    private Material getMaterial() {
+        return null;
+    }
 
     public static void main(String[] args) {
 	// write your code here
